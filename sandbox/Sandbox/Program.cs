92669bd94d29b0a953dd3fa6a60f09fa163c3029
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-08 01:50:24 +0200

    used variables and inputs in the playground file

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 4;
+        int y = 5;
+        int sum = x + y;
+        Console.WriteLine("The sum of x and y is: " + sum);
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+        Console.WriteLine("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");
     }
 }
