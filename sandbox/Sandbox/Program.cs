15b6711024fb62b2e1bf5b292667fcbf299ea21d
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-13 20:41:56 +0200

    get user to enter numbers

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -17,5 +17,13 @@ class Program
         int number = 42;
         string numberString = number.ToString();
         Console.WriteLine($"The number {number} is a string: {numberString}");
+
+        List<int> numbers = new List<int>(10); // the variable numbers holds the list and to use the list a new instance has to be created using the new keyword 10 is the initial capacity
+
+        List<string> words = new List<string>();
+        numbers.Add(2);
+        words.Add("mouse");
+
+        Console.WriteLine($"List {numbers[0]}");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numList = new List<int>(); //create a list to store the numbers
+        int userInput;
+        do
+        {
+            Console.Write("Enter a number to add to the list and 0 to stop adding: ");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput!= 0)
+            {
+                numList.Add(userInput);
+            }
+        } while (userInput != 0);
+
+        Console.WriteLine("");
     }
 }
