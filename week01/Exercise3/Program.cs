f6e61d91a396fdfbe933b94c5816d9bf69cae241
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-13 11:49:42 +0200

    Exercise 3 asked user for magic number and guess

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
     }
 }
