4d3a2c9ce57c9b75099ac7bdc1664e927a84ff4a
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-13 17:08:14 +0200

    add number of guesses counter

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,14 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Generate the magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);  // Random number between 1 and 100
+
         int userGuess;
+        int numberOfGuesses = 0; 
         
         do
         {
             Console.Write("What is your guess? ");
             userGuess = int.Parse(Console.ReadLine());
+            numberOfGuesses++;
             if (userGuess < magicNumber)
             {
                 Console.WriteLine("Higher");
