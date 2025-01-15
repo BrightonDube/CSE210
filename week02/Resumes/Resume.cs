c1fe7eaf136fddb2f5ee1f6cbb079401a25b6607
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-15 22:18:06 +0200

    modify the resume class to add the display method

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -8,10 +8,10 @@ namespace Resumes
     public class Resume
     {
         public string _name = "";
-        public List<Job> _jobs {get; set;} = new();
+        public List<Job> _jobs = new();
         public void DisplayResume()
         {   Console.WriteLine($"Name: {_name}");
-            Console.WriteLine("Jobs: ");
+            Console.WriteLine("Jobs:");
             foreach (Job job in _jobs)
             {   
                job.GetJob();
