b26822c0ae0802c6a5d1aa9aba997e1e3db578ac
Brighton Dube <BrightonDube@users.noreply.github.com>
2025-01-23 22:10:18 +0200

    Fix main program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,14 +5,15 @@ using System.Threading.Tasks;
 
 namespace ScriptureMemorizer
 {
-    public class Program
+    class Program 
     {
-        private static Reference r1 = new("Genesis", 1, 1, 5);
-        private static string _text = "In the beginning God Created Heaven and Earth";
-        private static Scripture s1 = new Scripture(r1, _text);
+        static void Main(string[] args)
+        {
+            Reference r1 = new("Genesis", 1, 1, 5);
+            string _text = "In the beginning God Created Heaven and Earth";
+            Scripture s1 = new Scripture(r1, _text);
 
-        private string isHidden = $"{s1.IsCompletelyHidden()}";
-
-       
+            string isHidden = $"{s1.IsCompletelyHidden()}";
+        }
     }
 }
