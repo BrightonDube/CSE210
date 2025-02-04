ad80978a55908717b1af1d4d113d556d05deb65e
Brighton <BrightonDube@users.noreply.github.com>
2025-02-04 15:01:04 +0200

    `Added properties and methods to Video class for title, author, length, comments and comment management.`

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -17,8 +17,32 @@ namespace YouTubeVideos
             _title = title;
             _author = author;
             _lenght = lenght;
+            _comments = new List<Comment>();
         }
 
+        public string GetTitle()
+        {
+            return _title;
+        }
+        public string GetAuthor()
+        {
+            return _author;
+            
+        }
+
+        public int GetLength()
+        {
+            return _lenght;
+        }
+        public void AddComment(Comment comment)
+        {
+            _comments.Add(comment);
+        }
+
+        public int GetNumberOfComments()
+        {
+            return _comments.Count;
+        }
         
     }
 }
