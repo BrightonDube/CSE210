3c0f4153f631ed151a2f92bf506df9d9b98ddeb3
Brighton <BrightonDube@users.noreply.github.com>
2025-02-04 14:51:47 +0200

    `Added private fields and constructor to Video class`

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -7,6 +7,18 @@ namespace YouTubeVideos
 {
     public class Video
     {
+        private string _title;
+        private string _author;
+        private int _lenght;
+        private List<Comment> _comments;
+
+        public Video(string title, string author, int lenght)
+        {
+            _title = title;
+            _author = author;
+            _lenght = lenght;
+        }
+
         
     }
 }
