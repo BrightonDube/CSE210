f80c5674893fac76dd0bf13acd1c85d12192b754
Brighton <BrightonDube@users.noreply.github.com>
2025-02-04 23:08:10 +0200

    `Added namespace and classes for Address and Customer, updated Program.cs to use them`

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
+namespace OnlineOrdering
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        public static void Main(string[] args)
+        {
+            Address usaAddress = new Address("123 Main St", "Los Angeles", "CA", "USA");
+            Address internationalAddress = new Address("456 Oxford Rd", "Johannesburg", "Gauteng", "South Africa");
+
+        
+            Customer customer1 = new Customer("John Smith", usaAddress);
+            Customer customer2 = new Customer("Jane Doe", internationalAddress);
+
+        }
+        
     }
 }
