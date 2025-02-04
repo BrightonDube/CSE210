8f8dba638e89ec712e53f1bb0f64d5fea71de4fa
Brighton <BrightonDube@users.noreply.github.com>
2025-02-05 00:50:07 +0200

    Created relevant classes

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mindfulness
+{
+    public class ReflectingActivity
+    {
+        List<string> _prompts;
+        List<string> _questions;
+
+        public ReflectingActivity()
+        {
+            _prompts = new List<string>();
+        }
+    }
+}
