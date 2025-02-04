30a77211a329c385e35ffd54825012b61125abfc
Brighton <BrightonDube@users.noreply.github.com>
2025-02-04 22:47:18 +0200

    `Added methods to Order class: AddProduct and CalculateTotalCost`

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -14,5 +14,17 @@ namespace OnlineOrdering
         _customer = customer;
         _products = new List<Product>();
        } 
+       public void AddProduct(Product product)
+       {
+        _products.Add(product);
+       }
+       public double CalculateTotalCost()
+       {
+        double productTotal = 0;
+        foreach (Product product in _products)
+        {
+            productTotal += product.CalculateProductTotal();
+        }
+       }
     }
 }
