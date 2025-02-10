a4e88ee5b4b18acd23d587354b43268dfe32d2df
Brighton <BrightonDube@users.noreply.github.com>
2025-02-10 14:11:42 +0200

    "Added new implementation for ListingActivity class with new features and functionality."

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -4,24 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mindfulness
-{
-    public class ListingActivity : Activity
     {
-        private int _count;
-        private List<string> _prompts;
-
-        ListingActivity(int count, List<string> prompts)
+        public class ListingActivity : Activity
         {
-            _count = count;
-            _prompts = prompts;
+            private static List<string> _prompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
+        };
+
+        private List<string> _items = new List<string>();
+        private Random _random = new Random();
+        private int _count;
+
+        public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+        {
         }
         public void Run()
         {
-            for (int i = 0; i < _count; i++)
-            {
-                Console.WriteLine(_prompts[i]);
+            DisplayStartingMessage();
+            SetDuration();
+            Console.Clear();
 
-            }
+            GetRandomPrompt();
+
+            Console.WriteLine("Get ready to begin listing in...");
+            ShowCountDown(5); // Pause for 5 seconds
+
+            Console.WriteLine("Begin!");
+            DateTime startTime = DateTime.Now;
+            DateTime futureTime = startTime.AddSeconds(_duration);
+
+            _items = GetListFromUser(futureTime);
+
+            _count = _items.Count;
+            Console.WriteLine($"You listed {_count} items!");
+            DisplayEndingMessage();
 
         }
     }
