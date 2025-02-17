da7001d3465ce3f83a33d2dcdc369da2ee8de5bd
Brighton <BrightonDube@users.noreply.github.com>
2025-02-17 22:51:24 +0200

    `Added constructor and RecordEvent method to EternalGoal class`

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,12 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EternalQuest
 {
     public class EternalGoal : Goal
     {
-        
+        public EternalGoal(string name, string description, int points) : base(name, description, points)
+        {
+
+        }
+        public override void RecordEvent()
+        {
+            Console.WriteLine($"You have recorded progress on the eternal goal: {GetShortName()}");
+        }
     }
 }
