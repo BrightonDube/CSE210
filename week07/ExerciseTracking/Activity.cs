a7c83b6f93a71644874d75325a2f82ede89fd7c6
Brighton <BrightonDube@users.noreply.github.com>
2025-02-18 01:46:12 +0200

    `Refactor Activity class and its subclasses to include date and duration properties, and add getter and setter methods for these properties. Update Program.cs to use the new properties and methods.`

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -4,13 +4,39 @@ namespace ExerciseTracking
 {
     public abstract class Activity
     {
-        private int _time;
+        private DateTime _date;
+        private int _durationMinutes;
 
-        public Activity(int time)
+        public Activity(DateTime date, int durationMinutes)
         {
-            _time = time;
+            _date = date;
+            _durationMinutes = durationMinutes;            
         }
-        public abstract int CalculateSpeed();
-        public abstract string GetSummary();
+        public DateTime GetDate()
+        {
+            return _date;
+        }
+        public void SetDate(DateTime date)
+        {
+            _date = date;
+        }
+        public int GetDurationMinutes()
+        {
+            return _durationMinutes;
+        }
+        public void SetDurationMinutes(int durationMinutes)
+        {
+            _durationMinutes = durationMinutes;
+        }
+
+        public virtual double CalculateSpeed()
+        {
+            return 0;
+        }
+        public virtual double GetPace(){return 0;}
+        public virtual string GetSummary()
+        {
+            return $"{_date:dd MMM yyyy} Activity ({_durationMinutes}) min";
+        }
     }
 }
--- week07/ExerciseTracking/SwimmingActivity.cs
+++ week07/ExerciseTracking/SwimmingActivity.cs
@@ -4,6 +4,19 @@ namespace ExerciseTracking
 {
     public class SwimmingActivity : Activity
     {
-        
-    }
+        private int _laps;
+
+        public SwimmingActivity(DateTime date, int durationMinutes, int laps) : base(date, durationMinutes)
+        {
+            _laps = laps;
+        }
+        public int GetLaps()
+        {
+            return _laps;
+        }
+        public void SetLaps(int laps)
+        {
+            _laps = laps;
+        }
+     }
 }
