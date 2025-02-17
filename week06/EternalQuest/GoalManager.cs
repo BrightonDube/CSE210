f3d3caa348ad25d4c2c4ecc5fde8a9b57f78e16d
Brighton <BrightonDube@users.noreply.github.com>
2025-02-17 23:42:42 +0200

    `Added Start method and DisplayPlayerInfo method to GoalManager class`

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -14,6 +14,54 @@ namespace EternalQuest
             _goals = new List<Goal>();
             _score = 0;
         }
-        
+        public void Start()
+        {
+            string choice = "";
+            while (choice != "6")
+            {
+                Console.WriteLine($"You have {_score} points.\n");
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("  1. Create New Goal");
+                Console.WriteLine("  2. List Goals");
+                Console.WriteLine("  3. Save Goals");
+                Console.WriteLine("  4. Load Goals");
+                Console.WriteLine("  5. Record Event");
+                Console.WriteLine("  6. Quit");
+                Console.Write("Select a choice from the menu: ");
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateGoal();
+                        break;
+                    case "2":
+                        ListGoalDetails();
+                        break;
+                    case "3":
+                        SaveGoals();
+                        break;
+                    case "4":
+                        LoadGoals();
+                        break;
+                    case "5":
+                        RecordEvent();
+                        break;
+                    case "6":
+                        Console.WriteLine("Exiting program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+         public void DisplayPlayerInfo()
+        {
+            Console.WriteLine($"Player Score: {_score}");
+        }
+
+
+
     }
 }
