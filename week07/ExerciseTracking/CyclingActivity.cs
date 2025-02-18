40dc41de9a8691760eb538ab80006d6b6b8ab767
Brighton <BrightonDube@users.noreply.github.com>
2025-02-18 09:32:27 +0200

    `Added properties and constructor to CyclingSpeed class in ExerciseTracking project.`

--- week07/ExerciseTracking/CyclingActivity.cs
+++ week07/ExerciseTracking/CyclingActivity.cs
@@ -4,6 +4,19 @@ namespace ExerciseTracking
 {
     public class CyclingSpeed : Activity
     {
-        
+        private double _speedKph;
+        public CyclingActivity(DateTime date, int durationMinutes, double speedKph) : base(date, durationMinutes)
+        {
+            _speedKph = speedKph;
+        }
+        public double GetSpeedKph()
+        {
+            return _speedKph;
+        }
+        public void SetSpeedKph(double speedKph)
+        {
+            _speedKph = speedKph;
+        }
+
     }
 }
