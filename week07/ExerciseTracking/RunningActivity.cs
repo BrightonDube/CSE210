485cfdd84f3579ed92f383d7aa626ecfcb7a6216
Brighton <BrightonDube@users.noreply.github.com>
2025-02-18 09:24:37 +0200

    `Refactor RunningActivity.cs: update constructor, add GetSummary method, and remove redundant code`

--- week07/ExerciseTracking/RunningActivity.cs
+++ week07/ExerciseTracking/RunningActivity.cs
@@ -7,35 +7,39 @@ namespace ExerciseTracking
         public RunningActivity()
         {
             private double _distanceKilometers;
-            public RunningActivity(DateTime date, int durationMinutes, double _distanceKilometers) : base(date, durationMinutes)
-            {
-                _distanceKilometers = _distanceKilometers;
-            }
-
-            public double GetDistanceKilometers()
-             {
-                return _distanceKilometers;
-             }
-            public void SetDistanceKilometers(double distanceKilometers)
-            {
-                _distanceKilometers = distanceKilometers;
-            }
-
-            public override double GetDistance()
-            {
-                return _distanceKilometers;
-            }
-            public override double GetSpeed()
-            {
-                return (GetDistanceKilometers() / GetDurationMinutes()) * 60;
-            }
-            public override double GetPace() 
-            {
-                return GetDurationMinutes() / GetDistanceKilometers();
-            }
-
+        public RunningActivity(DateTime date, int durationMinutes, double _distanceKilometers) : base(date, durationMinutes)
+        {
+            _distanceKilometers = _distanceKilometers;
         }
 
+        public double GetDistanceKilometers()
+        {
+            return _distanceKilometers;
+        }
+        public void SetDistanceKilometers(double distanceKilometers)
+        {
+            _distanceKilometers = distanceKilometers;
+        }
+
+        public override double GetDistance()
+        {
+            return _distanceKilometers;
+        }
+        public override double GetSpeed()
+        {
+            return (GetDistanceKilometers() / GetDurationMinutes()) * 60;
+        }
+        public override double GetPace()
+        {
+            return GetDurationMinutes() / GetDistanceKilometers();
+        }
+        public override string GetSummary()
+        {
+            return $"{GetDate(): dd MMM yyyy} Running ({GetDurationMinutes()} min): Distance {GetDistance(): F2} km, Speed {GetSpeed(): F2} km/h), Pace: {GetPace(): F2} min per km";
+        }
 
     }
+
+
+}
 }
